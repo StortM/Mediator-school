2edb39fae3396a86f2ae914e6705c85fb6a0fe30
TPD <mig.mic@gmail.com>
2020-12-08 15:23:23 +0100

    Menu v1

--- src/com/company/menu/Menu.java
+++ src/com/company/menu/Menu.java
@@ -1,52 +1,89 @@
 package com.company.menu;
 
-import com.company.Mediator;
 import com.company.MediatorImpl;
-import com.company.component.Button;
+import com.company.model.User;
 
 import java.util.Scanner;
 
-public class Menu implements Mediator{
+public class Menu {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
-MediatorImpl mediator;
-Button button;
+    MediatorImpl mediator;
+    int windowX = 150;
+    int windowY = 100;
+    String title = "Start title";
 
 
     public void startMenu() {
-
-        // consoleOutput.welcome
-        notify(mediator, "welcome");
-
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        //button.buttonPress();
-
-        switch (input) {
-            case 1-> System.out.println("create user"); // notify(mediator,"create") or logInMenu(sc);
-            case 2-> System.out.println("Log in"); //notify(mediator,"login") or createMenu(sc);
-            case 3-> {
-                notify(mediator,"exit");
-                System.out.println("exit");
-                break;
-            }
-
-        }
-
+        System.out.println("The title of the frame is " + title);
+        System.out.println("The size of this frame is " + windowX + " wide and " + windowY + " high");
     }
 
-public void logInMenu(Scanner sc){
-        notify(mediator, "printLoginMenu");
-
-}
-public void createUserMenu(Scanner sc){
-        notify(mediator,"createUser");
-
-}
-
-
-
-    @Override
-    public void notify(Object sender, String event) {
-
+    public void createUserMenu() {
+        System.out.println("createUser");
+        Scanner scanner = new Scanner(System.in);
+        User tempUser = new User();
+        System.out.println("Welcome new user \n Please enter your firstname...");
+        String input = scanner.nextLine();
+        tempUser.setFirstName(input);
+        System.out.println("\n Please enter your Lastname...");
+        input = scanner.nextLine();
+        tempUser.setLastName(input);
+        tempUser.setUsername(tempUser.getFirstName() + tempUser.getLastName() + (Math.random() * 35));
+        // If-check om Username er i DB
+        System.out.println("\n Please enter your Email adresse...");
+        input = scanner.nextLine();
+        // If-check om Email er i DB
+        tempUser.setEmail(input);
+        System.out.println("\n Please enter your Email adresse...");
+        input = scanner.nextLine();
+        // if-check if less than X chars
+        tempUser.setPassword(input);
+        // Send tempUser to MockDB
+        System.out.println("\n Welcome "+ ANSI_RED +tempUser.getFirstName() + ANSI_RESET + " to The Mediator Club!");
+        scanner.close();
+        // MainMenu(tempUser) ?
     }
+
+    public void logInMenu() {
+        System.out.println("loginmenu");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome \n Please enter your Username...");
+        String input = scanner.nextLine();
+        User tempUser = new User();
+        tempUser.setUsername(input);
+        System.out.println("\n Please enter your Password...");
+        input = scanner.nextLine();
+        tempUser.setPassword(input);
+        // Sendt tempUser to DB , check if els "Wrong information, Try again... logInMenu()
+        System.out.println("Welcome back "+ ANSI_RED +tempUser.getFirstName() + ANSI_RESET);
+        // MainMenu(tempUser) ?
+    }
+
+
+    public int getWindowX() {
+        return windowX;
+    }
+
+    public void setWindowxX(int windowxX) {
+        this.windowX = windowxX;
+    }
+
+    public int getWindowY() {
+        return windowY;
+    }
+
+    public void setWindowY(int windowY) {
+        this.windowY = windowY;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
--- src/com/company/model/User.java
+++ src/com/company/model/User.java
@@ -11,6 +11,11 @@ public class User {
     public User() {
     }
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public User(long id, String firstName, String lastName, String username, String password, String email) {
         this.id = id;
         this.firstName = firstName;
